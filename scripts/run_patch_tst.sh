cf2483b456591dc4f8bcb2c71ddd85f2a49306de
huliara <aaa402262@gmail.com>
2026-03-28 14:01:47 +0900

    fix: revise run_patch_tst.sh

--- scripts/run_patch_tst.sh
+++ scripts/run_patch_tst.sh
@@ -16,20 +16,20 @@ N_TIMESTEPS="${N_TIMESTEPS:-60000}"
 DT="${DT:-0.01}"
 H="${H:-0.01}"
 DATA_CMDS=(
-    (logistic-map --n-timesteps "$N_TIMESTEPS")
-    (henon-map --n-timesteps "$N_TIMESTEPS")
-    (lorenz --n-timesteps "$N_TIMESTEPS" --h "$H")
-    (lorenz96 --total-steps "$N_TIMESTEPS" --dt "$DT" --h "$H")
-    (rossler --n-timesteps "$N_TIMESTEPS" --h "$H")
-    (double-scroll --n-timesteps "$N_TIMESTEPS" --h "$H")
-    (multi-scroll --n-timesteps "$N_TIMESTEPS" --h "$H")
-    (rabinovich-fabrikant --n-timesteps "$N_TIMESTEPS")
-    (mackey-glass --n-timesteps "$N_TIMESTEPS")
-    (narma --n-timesteps "$N_TIMESTEPS")
-    (kuramoto-sivashinsky --n-timesteps "$N_TIMESTEPS")
+    "logistic-map --n-timesteps $N_TIMESTEPS"
+    "henon-map --n-timesteps $N_TIMESTEPS"
+    "lorenz --n-timesteps $N_TIMESTEPS --h $H"
+    "lorenz96 --total-steps $N_TIMESTEPS --dt $DT --h $H"
+    "rossler --n-timesteps $N_TIMESTEPS --h $H"
+    "double-scroll --n-timesteps $N_TIMESTEPS --h $H"
+    "multi-scroll --n-timesteps $N_TIMESTEPS --h $H"
+    "rabinovich-fabrikant --n-timesteps $N_TIMESTEPS"
+    "mackey-glass --n-timesteps $N_TIMESTEPS"
+    "narma --n-timesteps $N_TIMESTEPS"
+    "kuramoto-sivashinsky --n-timesteps $N_TIMESTEPS"
 )
 cd main
-for system in ${DATA_CMDS[@]}
+for system in "${DATA_CMDS[@]}"
  do
     cargo run -p main -- \
         --num-epochs "$NUM_EPOCHS" \
@@ -41,5 +41,5 @@ for system in ${DATA_CMDS[@]}
         --label-len "$LABEL_LEN" \
         --pred-len "$PRED_LEN" \
         "${MODEL_CMD[@]}" \
-        "${system[@]}"
+        $system
 done
